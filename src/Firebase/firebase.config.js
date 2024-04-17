3a6ff9eb55d4bd3c8ba6479da2c8ab50fe323165
apponislame <11appon11@gmail.com>
2024-04-17 14:45:39 +0600

    firebase config updated

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCQgna-0GiXSS9eWboRO5qs6VOxnj8PzpE",
-    authDomain: "assignmentb9a9.firebaseapp.com",
-    projectId: "assignmentb9a9",
-    storageBucket: "assignmentb9a9.appspot.com",
-    messagingSenderId: "20645081129",
-    appId: "1:20645081129:web:b9ae95a34ce4237a316c38",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
